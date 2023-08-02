aaea5cfeae9d9e29562bce4bf7170314bf94bb67
Yimin Yang <yvonne.yangh@gmail.com>
2023-08-02 18:58:29 -0500

    update views
    update views

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# request-> response
+# request handler
+# action
+def say_hello(request):
+    return render(request,'hello.html',{'name':'Mosh'})
+    # Pull data from db
+    # Transform
+    # Send email
